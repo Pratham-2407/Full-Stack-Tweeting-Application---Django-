376f48985b6c80110f503ce4c005bde0d23cc99c
Pratham-2407 <prathamgajapure24@gmail.com>
2024-07-13 18:18:09 +0530

    Created a test template by creating a simple index.html file and modifying required settings in various files to check if the tweet app is running properly

--- .history/chaihq/chaihq/urls_20240713180953.py
+++ .history/chaihq/chaihq/urls_20240713180953.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls')),
+    path('tweet/', include),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
