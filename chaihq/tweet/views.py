9685b949ea3417ec6b94c8e2be66e9ba211b87b4
Pratham-2407 <prathamgajapure24@gmail.com>
2024-07-14 18:29:30 +0530

    Added decorators before some functions in views.py file so that user should be logged in before creating, editing or deleting the tweet

--- chaihq/tweet/views.py
+++ chaihq/tweet/views.py
@@ -3,6 +3,7 @@ from .models import Tweet
 from .forms import TweetForm
 # , UserRegistrationForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -14,7 +15,7 @@ def tweet_list(request):
   tweets = Tweet.objects.all().order_by('-created_at')
   return render(request, 'tweet_list.html', {'tweets': tweets})
 
-
+@login_required
 def tweet_create(request):
   if request.method == "POST":
     form = TweetForm(request.POST, request.FILES)
@@ -27,7 +28,7 @@ def tweet_create(request):
     form = TweetForm()
   return render(request, 'tweet_form.html', {'form': form})
 
-
+@login_required
 def tweet_edit(request, tweet_id):
   tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
   if request.method == 'POST':
@@ -41,7 +42,7 @@ def tweet_edit(request, tweet_id):
     form = TweetForm(instance=tweet)
   return render(request, 'tweet_form.html', {'form': form})
 
-
+@login_required
 def tweet_delete(request, tweet_id):
   tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
   if request.method == 'POST':
